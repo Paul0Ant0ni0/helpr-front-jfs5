2e38ade6568261deac01cbaa05771a107a503bef
Paulo Antonio <dev.pauloantonio785@gmail.com>
2022-12-13 15:51:53 -0300

    Corrido erro de importação do template cargo.component.html

--- src/app/views/cargos/cargos.module.ts
+++ src/app/views/cargos/cargos.module.ts
@@ -1,12 +1,9 @@
-
 import { CargosRoutingModule } from './cargos-routing.module';
 import { CargosComponent } from './cargos/cargos.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from './../../shared/material/material.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ComponentsModule } from 'src/app/components/components.module';
 import { NewCargoComponent } from './new-cargo/new-cargo.component';
